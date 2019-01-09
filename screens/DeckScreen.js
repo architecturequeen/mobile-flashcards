d424ce1448ef2e1c45e2b8f8c2c0af25273598e7
architecturequeen <olawunmitaylor@gmail.com>
2019-01-09 23:05:08 +0000

    feat: update deck screen

--- screens/DeckScreen.js
+++ screens/DeckScreen.js
@@ -1,14 +1,5 @@
 import React from 'react';
-import {
-  Button,
-  Image,
-  Platform,
-  ScrollView,
-  StyleSheet,
-  Text,
-  TouchableOpacity,
-  View
-} from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default class DeckScreen extends React.Component {
   render() {
@@ -16,23 +7,36 @@ export default class DeckScreen extends React.Component {
 
     const { navigate } = this.props.navigation;
     return (
-      <ScrollView style={styles.container}>
-        <Text>{deck.title}</Text>
-        <Text>{deck.questions.length} cards</Text>
-        <Button title="Add Card TODO" onPress={() => {}} />
+      <View style={styles.container}>
+        <Text style={styles.text}>{deck.title} Deck!</Text>
+        <Text style={styles.text}>{deck.questions.length} cards</Text>
+        <Button
+          title="Add Card"
+          onPress={() => navigate('NewQuestion', { deckTitle: deck.title })}
+        />
         <Button
           title="Start Quiz"
           onPress={() => navigate('Quiz', { deck: deck })}
+          disabled={deck.questions.length === 0}
         />
-      </ScrollView>
+        <Button
+          title="Back to Home"
+          onPress={() => navigate('Home', { refresh: 'refresh' })}
+        />
+      </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-    // flex: 1,
-    // paddingTop: 15,
-    backgroundColor: '#fff'
+    paddingTop: 70,
+    backgroundColor: '#fff',
+    color: 'red'
+  },
+  text: {
+    fontSize: 20,
+    paddingBottom: 13,
+    textAlign: 'center'
   }
 });
